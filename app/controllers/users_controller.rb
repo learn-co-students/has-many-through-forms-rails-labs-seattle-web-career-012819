b67dac20ac4e1f9059f89fed27553676846dcdf4
tiefengeist <sahirnambiar@gmail.com>
2019-02-27 13:57:05 -0800

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+  end
 end
